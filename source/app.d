b4600de009d533508321d86985108dd05cc62620
Hiroki Noda <kubo39@gmail.com>
2019-03-08 13:46:38 +0900

    足し算と引き算を追加

--- source/app.d
+++ source/app.d
@@ -9,11 +9,32 @@ extern (C) int main(size_t argc, const(char)** argv)
         exit(EXIT_FAILURE);
     }
 
+    const(char)* p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
-    printf("  ret\n");
+    printf("  mov rax, %d\n", strtol(p, &p, 10));
 
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-')
+        {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("  ret\n");
     return EXIT_SUCCESS;
 }
